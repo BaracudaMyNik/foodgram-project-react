f346628726c947e89024dc71c9bbb8d49f6db6fd
Сергей Постнов <toosergey@yandex.ru>
2023-09-24 09:56:14 +0300

    Seven 7

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -1,14 +1,14 @@
 import os
 
-from dotenv import load_dotenv
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
 
 load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = get_random_secret_key()
-#SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+
 
 DEBUG = False
 
