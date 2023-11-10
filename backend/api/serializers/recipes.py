46a7900c6af5ccaa83b7dc70d48f35e6cc5163c6
Сергей Постнов <toosergey@yandex.ru>
2023-11-10 16:23:39 +0300

    Review edit user 05

--- backend/api/serializers/recipes.py
+++ backend/api/serializers/recipes.py
@@ -20,7 +20,7 @@ class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
-        fields = '__all__'
+        fields = ("id", "name", "color", "slug")
 
 
 class IngredientSerializer(serializers.ModelSerializer):
@@ -28,7 +28,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ingredient
-        fields = '__all__'
+        fields = ("id", "name", "measurement_unit")
 
 
 class IngredientAmountSerializer(serializers.ModelSerializer):
