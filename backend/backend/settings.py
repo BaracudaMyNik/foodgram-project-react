b5aa186f361d2e1b60e3a57320e1572ef3efd205
Сергей Постнов <toosergey@yandex.ru>
2023-11-11 14:37:05 +0300

    New VM-02

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'djoser',
     'api.apps.ApiConfig',
     'users.apps.UsersConfig',
